fbd71b8422a9a2b03b867609aeb55ebd9c757615
HAKOON0215 <hasung03@gmail.com>
2026-07-28 14:49:35 +0900

    디자인 옵션 선택 영역 제거

--- src/components/customize/DetailStep.tsx
+++ src/components/customize/DetailStep.tsx
@@ -1,26 +1,5 @@
 
-import { useDetailText } from "./detail/useDetailText";
 import { DetailInput } from "./detail/DetailInput";
-import { StyleSelect } from "./detail/StyleSelect";
-import { FitSelect } from "./detail/FitSelect";
-import { PocketSelect } from "./detail/PocketSelect";
-import { ColorSelect } from "./detail/ColorSelect";
-import { TextureSelect } from "./detail/TextureSelect";
-import { ElasticitySelect } from "./detail/ElasticitySelect";
-import { TransparencySelect } from "./detail/TransparencySelect";
-import { ThicknessSelect } from "./detail/ThicknessSelect";
-import { SeasonSelect } from "./detail/SeasonSelect";
-import { 
-  styleOptions, 
-  pocketOptions, 
-  colorOptions, 
-  fitOptions,
-  textureOptions,
-  elasticityOptions,
-  transparencyOptions,
-  thicknessOptions,
-  seasonOptions
-} from "@/lib/customize-constants";
 
 interface DetailStepProps {
   detailInput: string;
@@ -47,110 +26,21 @@ interface DetailStepProps {
 
 export const DetailStep = ({
   detailInput,
-  selectedStyle,
-  selectedPocket,
-  selectedColor,
-  selectedFit = "",
-  selectedTexture = "",
-  selectedElasticity = "",
-  selectedTransparency = "",
-  selectedThickness = "",
-  selectedSeason = "",
   onDetailInputChange,
-  onStyleSelect,
-  onPocketSelect,
-  onColorSelect,
-  onFitSelect = () => {},
-  onTextureSelect = () => {},
-  onElasticitySelect = () => {},
-  onTransparencySelect = () => {},
-  onThicknessSelect = () => {},
-  onSeasonSelect = () => {},
 }: DetailStepProps) => {
-  const { handleTextAreaChange } = useDetailText({
-    detailInput,
-    selectedStyle,
-    selectedPocket,
-    selectedColor,
-    selectedFit,
-    selectedTexture,
-    selectedElasticity,
-    selectedTransparency,
-    selectedThickness,
-    selectedSeason,
-    styleOptions,
-    pocketOptions,
-    colorOptions,
-    fitOptions,
-    textureOptions,
-    elasticityOptions,
-    transparencyOptions,
-    thicknessOptions,
-    seasonOptions,
-    onDetailInputChange,
-  });
+  const handleTextAreaChange = (
+    event: React.ChangeEvent<HTMLTextAreaElement>,
+  ) => {
+    onDetailInputChange(event.target.value);
+  };
 
   return (
-    <div className="space-y-8">
-      {/* Detail Input Area */}
-      <DetailInput 
-        detailInput={detailInput} 
-        onChange={handleTextAreaChange} 
+    <div className="mx-auto w-full max-w-3xl">
+      <DetailInput
+        detailInput={detailInput}
+        onChange={handleTextAreaChange}
+        onExampleUse={onDetailInputChange}
       />
-
-      {/* Detail Options */}
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-        {/* First Row */}
-        <StyleSelect 
-          selectedStyle={selectedStyle} 
-          styleOptions={styleOptions} 
-          onStyleSelect={onStyleSelect} 
-        />
-        <FitSelect 
-          selectedFit={selectedFit} 
-          fitOptions={fitOptions} 
-          onFitSelect={onFitSelect} 
-        />
-        <PocketSelect 
-          selectedPocket={selectedPocket} 
-          pocketOptions={pocketOptions} 
-          onPocketSelect={onPocketSelect} 
-        />
-        
-        {/* Second Row */}
-        <ColorSelect 
-          selectedColor={selectedColor} 
-          colorOptions={colorOptions} 
-          onColorSelect={onColorSelect} 
-        />
-        <TextureSelect
-          selectedTexture={selectedTexture}
-          textureOptions={textureOptions}
-          onTextureSelect={onTextureSelect}
-        />
-        <ElasticitySelect
-          selectedElasticity={selectedElasticity}
-          elasticityOptions={elasticityOptions}
-          onElasticitySelect={onElasticitySelect}
-        />
-        
-        {/* Third Row */}
-        <TransparencySelect
-          selectedTransparency={selectedTransparency}
-          transparencyOptions={transparencyOptions}
-          onTransparencySelect={onTransparencySelect}
-        />
-        <ThicknessSelect
-          selectedThickness={selectedThickness}
-          thicknessOptions={thicknessOptions}
-          onThicknessSelect={onThicknessSelect}
-        />
-        <SeasonSelect
-          selectedSeason={selectedSeason}
-          seasonOptions={seasonOptions}
-          onSeasonSelect={onSeasonSelect}
-        />
-      </div>
     </div>
   );
 };

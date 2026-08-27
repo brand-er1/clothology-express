66f812322d994067b9232a9c89737602af5e29fb
HAKOON0215 <hasung03@gmail.com>
2026-08-27 15:38:43 +0900

    Mount mascot wait control

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
+import { MascotWaitControl } from './components/guide/MascotWaitControl.tsx'
 import './index.css'
 
 // Strictly ensure we find the root element
 const rootElement = document.getElementById("root");
 if (!rootElement) throw new Error("Failed to find the root element");
 
-createRoot(rootElement).render(<App />);
+createRoot(rootElement).render(
+  <>
+    <App />
+    <MascotWaitControl />
+  </>
+);
